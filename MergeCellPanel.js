35af75688b0a94434e91c2e3d0a88f8d4174229d
Tan Xiliang <x531tanxl@foxmail.com>
2015-08-04 17:13:53 +0800

    分离2个模块

---
+++ MergeCellPanel.js
@@ -0,0 +1,23 @@
+﻿
+Ext.define('Ext.ux.grid.MergeCellPanel', {
+    extend: 'Ext.grid.Panel',
+    
+    alias: ['widget.mergecellgrid'],
+    viewType: 'mergecelltableview',
+    
+    initComponent: function() {
+        var me = this;
+        
+        Ext.apply(me, {
+            selType: 'cellmodel', // 按单元格选择
+            sortableColumns: false, // 禁止排序
+            columnLines: true, // 显示网格线
+            rowLines: true, // 显示网格线
+            trackMouseOver: false // 禁止跟踪鼠标变色
+        });
+        
+        me.callParent();
+    }
+});
+
+
